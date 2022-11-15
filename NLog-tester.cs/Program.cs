1a67823c7d8bc6f2e4bb0d2744311d5e6d6cf786
osawa-koki <github@osawa.dev>
2022-11-15 19:33:04 +0900

    fst commit;;

---
+++ NLog-tester.cs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NLogSample
+{
+  class Program
+  {
+    static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+    static void Main(string[] args)
+    {
+      logger.Trace("Trace ログです。");
+      logger.Debug("Debug ログです。");
+      logger.Info("Info ログです。");
+      logger.Warn("Warn ログです。");
+      logger.Error("Error ログです。");
+      logger.Fatal("Fatal ログです。");
+
+      try
+      {
+        var a = 0;
+        Console.WriteLine(10 / a);  // エラーを発生させる
+      }
+      catch (Exception ex)
+      {
+        logger.Error(ex, "エラーが発生しました。");
+      }
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# NLog-tester.cs
+
+NLogの学習用プロジェクト。  
+
+## 参考文献
+
+- <https://sabakunotabito.hatenablog.com/entry/2021/10/25/005740>
